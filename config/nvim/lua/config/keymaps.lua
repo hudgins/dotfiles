4efc5b563d0f32a0721e29d6c1a2595156a98522
Allan Hudgins <allan@retreat.guru>
2025-03-17 08:27:01 -0700

    Save with <space><space> and CMD-s, and :wall variants

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -15,3 +15,9 @@ vim.keymap.set('i', '<D-v>', '<ESC>"+p', { desc = "Paste from system clipboard" 
 vim.keymap.set('n', '<leader>gb', '<cmd>Gitsigns blame_line<CR>', { desc = "Git blame line" })
 vim.keymap.set('n', '<leader>gB', function() Snacks.git.blame_line() end, { desc = "Git blame line - bigger" })
 vim.keymap.set({'n', 'v'}, '<leader>gH', function() Snacks.gitbrowse() end, { desc = "Git Browse" })
+
+-- save
+vim.keymap.set('n', '<D-s>', '<cmd>update<CR>', { desc = "Save file if changed" })
+vim.keymap.set('n', '<leader><space>', '<cmd>update<CR>', { desc = "Save file if changed" })
+vim.keymap.set('n', '<D-S-s>', '<cmd>wall<CR>', { desc = "Save all changed files" })
+vim.keymap.set('n', '<leader><enter>', '<cmd>wall<CR>', { desc = "Save all changed files" })
