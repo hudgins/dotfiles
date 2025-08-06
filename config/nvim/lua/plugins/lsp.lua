910efb65b18ecec1550f36f9c5643ab739a499b7
Allan Hudgins <allan@retreat.guru>
2025-08-06 16:43:09 -0700

    Add PICO-8 LSP and glow config and hide mouse pointer in Ghostty

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,3 +1,28 @@
+-- vim.api.nvim_create_autocmd({'BufNew', 'BufEnter'}, {
+--     pattern = { '*.p8' },
+--     callback = function(args)
+--         vim.lsp.start({
+--             name = 'pico8-ls',
+--             cmd = { 'pico8-ls', '--stdio' },
+--             root_dir = vim.fs.dirname(vim.api.nvim_buf_get_name(args.buf)),
+--             -- Setup your keybinds in the on_attach function
+--             on_attach = on_attach,
+--         })
+--     end
+-- })
+local configs = require("lspconfig.configs")
+
+if not configs.pico8_ls then
+  configs.pico8_ls = {
+    default_config = {
+      cmd = { "pico8-ls", "--stdio" },
+      filetypes = { "pico8", "p8" },
+      root_dir = function(fname) return vim.fs.dirname(fname) end,
+      settings = {},
+    },
+  }
+end
+
 return {
   {
     "neovim/nvim-lspconfig",
@@ -18,4 +43,18 @@ return {
       },
     },
   },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        pico8_ls = {},  -- enable it
+      },
+      setup = {
+        pico8_ls = function(_, opts)
+          require("lspconfig").pico8_ls.setup(opts)
+          return true
+        end,
+      },
+    },
+  }
 }
